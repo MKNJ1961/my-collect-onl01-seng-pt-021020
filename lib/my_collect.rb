38ccab4d58c9123b9c878fa79b7f2b92589f89be
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-18 01:00:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,14 +3,9 @@ require 'pry'
 def my_collect(array) 
   i = 0
   name_collection = []
-  
-  
-  
-  
   while i < array.length
     name_collection.push yield(array[i])
-
-i += 1
+    i += 1
 end
 name_collection
 end
